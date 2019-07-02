5ba91f870cfa83778afd54761ef1bcb803fa0b54
MigMikael <chanachai_mig@hotmail.com>
2019-07-02 22:16:30 +0700

    init commit

---
+++ src/jokeData.js
@@ -0,0 +1,33 @@
+const jokesData = [
+    {
+        id: 1,
+        punchLine: "Bla bla bla"
+    },
+    {
+        id: 2,
+        question: "Q2",
+        punchLine: "Bla bla bla"
+    },
+    {
+        id: 3,
+        question: "Q3",
+        punchLine: "Bla bla bla"
+    },
+    {
+        id: 4,
+        question: "Q4",
+        punchLine: "Bla bla bla"
+    },
+    {
+        id: 5,
+        question: "Q5",
+        punchLine: "Bla bla bla"
+    },
+    {
+        id: 6,
+        question: "Q6",
+        punchLine: "Bla bla bla"
+    },
+]
+
+export default jokesData
